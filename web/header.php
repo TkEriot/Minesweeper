b58e90a7a572294609b58ba5a8074402fedfdd9a
TkEriot <TkEriot@users.noreply.github.com>
2017-07-09 11:06:56 +0200

    Add files via upload

---
+++ web/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+    <title>SweepCoins<?php if(isset($_GET['auth'])):?> - Play!<?php endif;?></title>
+    <link rel="icon" href="img/square.png" type="image/png" >
+    <meta name="title" content="SweepCoins.com" />
+    <meta name="description" content="Minesweeper game for bitcoin and litecoin, no registration needed, invite friends and have fun!" />
+    <meta name="keywords" content="bitcoin,litecoin,minesweeper,mines,game,sweeper,play,referrals,bomb,friends,fun" />
+    <link rel="stylesheet" href="css/semantic.min.css" type="text/css"/>
+    <link rel="stylesheet" href="css/main.css" type="text/css"/>
+    <script src="js/jquery-3.2.0.min.js" type="text/javascript"></script>
+    <script src="js/semantic.min.js" type="text/javascript"></script>
+    <script src="js/all.js" type="text/javascript"></script>
+</head>
+<body>
